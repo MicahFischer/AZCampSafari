f0ad0005be4bf7a37ecf5c9c5477f264304a9331
MicahFischer <43285001+MicahFischer@users.noreply.github.com>
2018-12-18 14:09:52 -0700

    Add files via upload

--- slideout.js
+++ slideout.js
@@ -9,7 +9,9 @@ function closeNav() {
 }
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction()
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -24,5 +26,3 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-
